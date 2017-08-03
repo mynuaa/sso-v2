2e54becf09aaf1f0e555c392b735fd5b21141b80
Rex Zeng <rex.fgtsky@qq.com>
2017-08-03 20:50:55 +0800

    [Checklist] 修改教务处验证的方法

--- backend/Services/NuaaVerify.php
+++ backend/Services/NuaaVerify.php
@@ -6,50 +6,47 @@
  */
 
 class NuaaVerify {
+    private $config;
     private function dedverify($stuid, $password) {
-        $url = "http://ded.nuaa.edu.cn/NetEAn/User/check.asp";
-        $post = "user=" . $stuid . "&pwd=" . $password;
-        // disable notice for tempnam
-        $level = error_reporting();
-        error_reporting($level & ~E_NOTICE);
-        $cookie = tempnam('/tmp', 'sso-v2-');
-        $newCookie = tempnam('/tmp', 'sso-v2-');
+        $cookie = @tempnam('/tmp', 'MYAUTH_');
         $curl = curl_init();
         curl_setopt_array($curl, [
-            CURLOPT_URL => $url,
-            CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => $post,
+            CURLOPT_NOBODY => true,
+            CURLOPT_URL => 'http://' . $this->config['site']['ded_host'] . '/NetEAn/User/login.asp',
             CURLOPT_COOKIEJAR => $cookie,
-            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_RETURNTRANSFER => true,
         ]);
         curl_exec($curl);
         curl_setopt_array($curl, [
-            CURLOPT_COOKIEFILE => $cookie,
-            CURLOPT_REFERER => 'http://ded.nuaa.edu.cn',
-            CURLOPT_COOKIEJAR => $newCookie,
+            CURLOPT_POST => true,
+            CURLOPT_URL => 'http://' . $this->config['site']['ded_host'] . '/NetEAn/User/check.asp',
+            CURLOPT_POSTFIELDS => 'user=' . $stuid . '&pwd=' . $password,
+            CURLOPT_REFERER => 'http://ded.nuaa.edu.cn/netean/user/login.asp',
+            CURLOPT_HTTPHEADER => [
+                'Origin: http://ded.nuaa.edu.cn',
+                'Content-type: application/x-www-form-urlencoded'
+            ],
+            CURLOPT_COOKIEFILE => $cookie
         ]);
         $response = curl_exec($curl);
         $success = strstr($response, 'switch (0){') != false;
+        $realname = false;
         if ($success) {
             $db = new DataBase();
             if (!$realname = $db->get('users', 'name', ['stu_num' => $stuid])) {
-                $url = 'http://ded.nuaa.edu.cn/netean/newpage/xsyh/title.asp';
                 curl_setopt_array($curl, [
-                    CURLOPT_URL => $url,
-                    CURLOPT_COOKIEFILE => $newCookie,
-                    CURLOPT_RETURNTRANSFER => 1,
+                    CURLOPT_URL => 'http://' . $this->config['site']['ded_host'] . '/netean/newpage/xsyh/title.asp',
+                    CURLOPT_COOKIEFILE => $cookie,
+                    CURLOPT_RETURNTRANSFER => true,
                 ]);
                 $result = iconv('GB2312', 'UTF-8', curl_exec($curl));
                 preg_match('/^.+\.(.+?)\).+$/s', $result, $arr);
                 $realname = $arr[1];
                 $db->update('users', ['name' => $realname], ['stu_num' => $stuid]);
             }
-        } else {
-            return false;
         }
         curl_close($curl);
         unlink($cookie);
-        unlink($newCookie);
         return $realname;
     }
     private function gsmverify($gsmid, $password) {
@@ -57,16 +54,16 @@ class NuaaVerify {
         $password = $password;
         $prepare_curl = curl_init();
         curl_setopt_array($prepare_curl, [
-            CURLOPT_URL => "http://gsmis.nuaa.edu.cn/pyxx/login.aspx",
-            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'http://' . $this->config['site']['gsm_host'] . '/pyxx/login.aspx',
+            CURLOPT_RETURNTRANSFER => true,
         ]);
         preg_match('/name="__VIEWSTATE" value=".+?"/', curl_exec($prepare_curl), $viewstate);
         $viewstate = substr($viewstate[0], 26);
         $viewstate = preg_replace('/"/', '', $viewstate);
         $viewstate = urlencode($viewstate);
         curl_close($prepare_curl);
-        $x = intval(rand(60));
-        $y = intval(rand(60));
+        $x = intval(rand(0, 60));
+        $y = intval(rand(0, 60));
         $post = "__VIEWSTATE={$viewstate}&_ctl0%3Atxtusername={$gsmid}&_ctl0%3AImageButton1.x={$x}&_ctl0%3AImageButton1.y={$y}&_ctl0%3Atxtpassword={$password}";
         $url = "http://gsmis.nuaa.edu.cn/pyxx/login.aspx";
         $curl = curl_init();
@@ -98,12 +95,13 @@ class NuaaVerify {
         return $response['status'] == 0;
     }
     public function verify($username, $password) {
+        $this->config = require __DIR__ . '/../config.php';
         $username = urlencode($username);
         $password = urlencode($password);
         return (
-            (preg_match("/(^7020|^LZ)/i", $username) && self::hrverify($username, $password)) ||
-            (preg_match("/(^SX|^SY|^SZ|^BX)/i", $username) && self::gsmverify($username, $password)) ||
-            self::dedverify($username, $password)
+            (preg_match("/(^7020|^LZ)/i", $username) && $this->hrverify($username, $password)) ||
+            (preg_match("/(^SX|^SY|^SZ|^BX)/i", $username) && $this->gsmverify($username, $password)) ||
+            $this->dedverify($username, $password)
         );
     }
 }
