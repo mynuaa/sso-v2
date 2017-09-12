af2fc66cd7b3e6e155fa4401a33e4ab622db67ae
Rex Zeng <rex.fgtsky@qq.com>
2017-09-12 19:37:29 +0800

    update sql file

--- docs/sso-v2.sql
+++ docs/sso-v2.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `sso-v2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `sso-v2`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sso-v2
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.17-MariaDB
+-- Server version	5.5.5-10.2.7-MariaDB-10.2.7+maria~jessie
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,19 +18,71 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user_tokens`
+-- Table structure for table `oauth_apps`
 --
 
-DROP TABLE IF EXISTS `user_tokens`;
+DROP TABLE IF EXISTS `oauth_apps`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_tokens` (
+CREATE TABLE `oauth_apps` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appid` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `appsecret` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `appname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `permissions` int(11) NOT NULL DEFAULT 1 COMMENT '状态压缩的二进制串',
+  `offical` int(11) NOT NULL DEFAULT 0,
+  `created` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `appid_UNIQUE` (`appid`),
+  UNIQUE KEY `appname_UNIQUE` (`appname`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth_apps`
+--
+
+LOCK TABLES `oauth_apps` WRITE;
+/*!40000 ALTER TABLE `oauth_apps` DISABLE KEYS */;
+INSERT INTO `oauth_apps` VALUES (1,'12345678','12345678','纸飞机电子办公系统',1,1,'2017-09-11');
+/*!40000 ALTER TABLE `oauth_apps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth_codes`
+--
+
+DROP TABLE IF EXISTS `oauth_codes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_codes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL,
-  `sid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`appid`,`user_id`),
+  KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oauth_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oauth_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appid` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `expires` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `sid` (`sid`)
+  UNIQUE KEY `UNIQUE` (`appid`,`user_id`),
+  KEY `content` (`content`),
+  KEY `appid` (`appid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -49,8 +103,26 @@ CREATE TABLE `user` (
   UNIQUE KEY `username_UNIQUE` (`username`),
   KEY `stu_num` (`stu_num`),
   KEY `openid` (`openid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='sso-v2 用户表。';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_tokens`
+--
+
+DROP TABLE IF EXISTS `user_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `sid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expires` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sid` (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -61,4 +133,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-19 16:26:32
+-- Dump completed on 2017-09-12 19:35:42
